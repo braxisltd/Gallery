a191b4198c523952022243da1c89ce64f97b13e0
Darren Ballinger <darren.ballinger@gmail.com>
2012-04-18 19:33:57 +0100

    Initial commit

---
+++ dist/run.sh
@@ -0,0 +1,1 @@
+java -classpath .:lib/gallery-1.0.jar:lib/freemarker-2.3.9.jar:lib/guava-11.0.jar:lib/jsr305-1.3.9.jar:lib/simple-4.1.21.jar:lib/logback-classic-1.0.1.jar:lib/logback-core-1.0.1.jar:lib/slf4j-api-1.6.4.jar com.braxisltd.gallery.application.RunProd
